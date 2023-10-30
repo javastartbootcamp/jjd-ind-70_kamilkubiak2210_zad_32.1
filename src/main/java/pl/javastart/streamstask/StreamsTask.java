bc784e6bdb4e1bc1f8613d64dd044f7b30867234
kamilkubiak2210 <117714298+kamilkubiak2210@users.noreply.github.com>
2023-10-30 17:18:18 +0100

    Rozwiązanie.

--- src/main/java/pl/javastart/streamstask/StreamsTask.java
+++ src/main/java/pl/javastart/streamstask/StreamsTask.java
@@ -1,10 +1,8 @@
 package pl.javastart.streamstask;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamsTask {
 
@@ -14,46 +12,67 @@ public class StreamsTask {
     }
 
     private void run() {
-        List<User> users = new ArrayList<>();
+        List<User> users = getUsers();
+        List<Expense> expenses = getExpenses();
 
+        Collection<User> women = findWomen(users);
+        Double averageMenAge = averageMenAge(users);
+        Map<Long, List<Expense>> expensesByUserId = groupExpensesByUserId(users, expenses);
+        Map<User, List<Expense>> expensesByUser = groupExpensesByUser(users, expenses);
+        System.out.println(women);
+        System.out.println(averageMenAge);
+        System.out.println(expensesByUserId);
+        System.out.println(expensesByUser);
+    }
+
+    private static List<User> getUsers() {
+        List<User> users = new ArrayList<>();
         users.add(new User(1L, "Alicja", 20));
         users.add(new User(2L, "Dominik", 15));
         users.add(new User(3L, "Patrycja", 25));
         users.add(new User(4L, "Marcin", 30));
         users.add(new User(5L, "Tomek", 18));
         users.add(new User(6L, "Damian", 26));
+        return users;
+    }
 
+    private static List<Expense> getExpenses() {
         List<Expense> expenses = new ArrayList<>();
         expenses.add(new Expense(1L, "Buty", new BigDecimal("149.99"), ExpenseType.WEAR));
         expenses.add(new Expense(1L, "Sałatka", new BigDecimal("14.99"), ExpenseType.FOOD));
         expenses.add(new Expense(2L, "Bluza", new BigDecimal("100"), ExpenseType.WEAR));
         expenses.add(new Expense(2L, "Skarpetki", new BigDecimal("39"), ExpenseType.WEAR));
         expenses.add(new Expense(2L, "Pizza", new BigDecimal("25"), ExpenseType.FOOD));
-
-        Collection<User> women = findWomen(users);
-        Double averageMenAge = averageMenAge(users);
-        Map<Long, List<Expense>> expensesByUserId = groupExpensesByUserId(users, expenses);
-        Map<User, List<Expense>> expensesByUser = groupExpensesByUser(users, expenses);
+        return expenses;
     }
 
     // metoda powinna zwracać listę kobiet (sprawdzając, czy imię kończy się na "a")
     Collection<User> findWomen(Collection<User> users) {
-        throw new RuntimeException("Not implemented");
+        return users.stream().filter(user -> user.getName().endsWith("a")).toList();
     }
 
     // metoda powinna zwracać średni wiek mężczyzn (sprawdzając, czy imię nie kończy się na "a")
     Double averageMenAge(Collection<User> users) {
-        throw new RuntimeException("Not implemented");
+        return users.stream().filter(user -> !user.getName().endsWith("a")).mapToDouble(User::getAge).average().orElse(0.0);
     }
 
     // metoda powinna zwracać wydatki zgrupowane po ID użytkownika
     Map<Long, List<Expense>> groupExpensesByUserId(Collection<User> users, List<Expense> expenses) {
-        throw new RuntimeException("Not implemented");
+        return expenses.stream().collect(Collectors.groupingBy(Expense::getUserId));
     }
 
     // metoda powinna zwracać wydatki zgrupowane po użytkowniku
     // podobne do poprzedniego, ale trochę trudniejsze
     Map<User, List<Expense>> groupExpensesByUser(Collection<User> users, List<Expense> expenses) {
-        throw new RuntimeException("Not implemented");
+        Map<Long, List<Expense>> groupExpensesByUserId = groupExpensesByUserId(users, expenses);
+
+        Map<User, List<Expense>> expensesByUser = new HashMap<>();
+        for (User user : users) {
+            List<Expense> userExpenses = groupExpensesByUserId.get(user.getId());
+            if (userExpenses != null) {
+                expensesByUser.put(user, userExpenses);
+            }
+        }
+        return expensesByUser;
     }
 }
--- src/test/java/pl/javastart/streamstask/ExampleTest.java
+++ src/test/java/pl/javastart/streamstask/ExampleTest.java
@@ -1,6 +1,124 @@
 package pl.javastart.streamstask;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+
 public class ExampleTest {
 
+    @Test
 
+    public void shouldReturnAllWomen() {
+        //given
+        StreamsTask streamsTask = new StreamsTask();
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(1L, "Alicja", 20));
+        users.add(new User(2L, "Dominik", 15));
+        users.add(new User(3L, "Patrycja", 25));
+        users.add(new User(4L, "Marcin", 30));
+        users.add(new User(5L, "Tomek", 18));
+        users.add(new User(6L, "Damian", 26));
+
+        //when
+        Collection<User> women = streamsTask.findWomen(users);
+        List<User> womenAssert = new ArrayList<>();
+        womenAssert.add(new User(1L, "Alicja", 20));
+        womenAssert.add(new User(3L, "Patrycja", 25));
+
+        //then
+        Assertions.assertEquals(womenAssert, women);
+
+    }
+
+    @Test
+    public void shouldReturnAverageMenAge() {
+        //given
+        StreamsTask streamsTask = new StreamsTask();
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(1L, "Alicja", 20));
+        users.add(new User(2L, "Dominik", 15));
+        users.add(new User(3L, "Patrycja", 25));
+        users.add(new User(4L, "Marcin", 30));
+        users.add(new User(5L, "Tomek", 18));
+        users.add(new User(6L, "Damian", 26));
+
+        //when
+        Double averageMenAge = streamsTask.averageMenAge(users);
+
+        //then
+        Assertions.assertEquals(22.25, averageMenAge);
+
+    }
+
+    @Test
+    public void shouldReturnGroupExpensesByUserId() {
+        //given
+        StreamsTask streamsTask = new StreamsTask();
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "Alicja", 20));
+        users.add(new User(2L, "Dominik", 15));
+        users.add(new User(3L, "Patrycja", 25));
+        users.add(new User(4L, "Marcin", 30));
+        users.add(new User(5L, "Tomek", 18));
+        users.add(new User(6L, "Damian", 26));
+        List<Expense> expenses = new ArrayList<>();
+        expenses.add(new Expense(1L, "Buty", new BigDecimal("149.99"), ExpenseType.WEAR));
+        expenses.add(new Expense(1L, "Sałatka", new BigDecimal("14.99"), ExpenseType.FOOD));
+        expenses.add(new Expense(2L, "Bluza", new BigDecimal("100"), ExpenseType.WEAR));
+        expenses.add(new Expense(2L, "Skarpetki", new BigDecimal("39"), ExpenseType.WEAR));
+        expenses.add(new Expense(2L, "Pizza", new BigDecimal("25"), ExpenseType.FOOD));
+
+        //when
+        Map<Long, List<Expense>> groupExpensesByUserId = streamsTask.groupExpensesByUserId(users, expenses);
+        Map<Long, List<Expense>> groupExpensesByUserIdAssert = new HashMap<>();
+        groupExpensesByUserIdAssert.put(1L,
+                List.of(new Expense(1L, "Buty", new BigDecimal("149.99"), ExpenseType.WEAR),
+                        new Expense(1L, "Sałatka", new BigDecimal("14.99"), ExpenseType.FOOD)));
+        groupExpensesByUserIdAssert.put(2L,
+                List.of(new Expense(2L, "Bluza", new BigDecimal("100"), ExpenseType.WEAR)
+                        , new Expense(2L, "Skarpetki", new BigDecimal("39")
+                                , ExpenseType.WEAR), new Expense(2L, "Pizza", new BigDecimal("25"), ExpenseType.FOOD)));
+
+        //then
+        Assertions.assertEquals(groupExpensesByUserIdAssert, groupExpensesByUserId);
+
+    }
+
+    @Test
+    public void shouldReturnGroupExpensesByUser() {
+        //given
+        StreamsTask streamsTask = new StreamsTask();
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "Alicja", 20));
+        users.add(new User(2L, "Dominik", 15));
+        users.add(new User(3L, "Patrycja", 25));
+        users.add(new User(4L, "Marcin", 30));
+        users.add(new User(5L, "Tomek", 18));
+        users.add(new User(6L, "Damian", 26));
+        List<Expense> expenses = new ArrayList<>();
+        expenses.add(new Expense(1L, "Buty", new BigDecimal("149.99"), ExpenseType.WEAR));
+        expenses.add(new Expense(1L, "Sałatka", new BigDecimal("14.99"), ExpenseType.FOOD));
+        expenses.add(new Expense(2L, "Bluza", new BigDecimal("100"), ExpenseType.WEAR));
+        expenses.add(new Expense(2L, "Skarpetki", new BigDecimal("39"), ExpenseType.WEAR));
+        expenses.add(new Expense(2L, "Pizza", new BigDecimal("25"), ExpenseType.FOOD));
+
+        //when
+        Map<User, List<Expense>> groupExpensesByUser = streamsTask.groupExpensesByUser(users, expenses);
+        Map<User, List<Expense>> groupExpensesByUserAssert = new HashMap<>();
+        groupExpensesByUserAssert.put(new User(1L, "Alicja", 20),
+                List.of(new Expense(1L, "Buty", new BigDecimal("149.99"), ExpenseType.WEAR),
+                        new Expense(1L, "Sałatka", new BigDecimal("14.99"), ExpenseType.FOOD)));
+        groupExpensesByUserAssert.put(new User(2L, "Dominik", 15),
+                List.of(new Expense(2L, "Bluza", new BigDecimal("100"), ExpenseType.WEAR),
+                        new Expense(2L, "Skarpetki", new BigDecimal("39"), ExpenseType.WEAR),
+                        new Expense(2L, "Pizza", new BigDecimal("25"), ExpenseType.FOOD)));
+
+        //then
+        Assertions.assertEquals(groupExpensesByUserAssert, groupExpensesByUser);
+
+    }
 }
